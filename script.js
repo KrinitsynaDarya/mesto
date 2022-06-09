be617bbedf2b735f584adcf6725f6d7a256af2ae
Darya Krinitsyna <krinitsyna.darya.98@mail.ru>
2022-06-09 08:16:30 +0300

    feat:карточки мест

--- script.js
+++ script.js
@@ -3,12 +3,21 @@ console.log('Hello, Dasha!');
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-button');
+
+
+
+
+
+let likeButtons = document.querySelectorAll('.element__like-button');
+console.log(likeButtons.length);
+
 let profileName = document.querySelector('.profile__name');
 
-let profileActivity = document.querySelector('.profile__activity');
+let profileActivity = document.querySelector('.profile__about');
 let fieldName = document.querySelector('.name');
-let fieldActivity = document.querySelector('.activity');
+let fieldActivity = document.querySelector('.about');
 let submitButton = document.querySelector('.popup__submit-button');
+let button;
 
 editButton.addEventListener('click', function () {
     popup.classList.remove('hidden');
@@ -16,12 +25,23 @@ editButton.addEventListener('click', function () {
     fieldActivity.value = profileActivity.textContent;
 })
 
+function addLike(button) {
+    button.classList.toggle('element__like-button_active');
+}
+
+
+likeButtons.forEach(function (likeButton) {
+    likeButton.addEventListener('click', () => addLike(likeButton)
+    );
+});
+
+
 closeButton.addEventListener('click', function () {
     popup.classList.add('hidden');
 })
 
 submitButton.addEventListener('click', function (evt) {
-    evt.preventDefault(); 
+    evt.preventDefault();
     let inputs = document.querySelectorAll('input');
     profileName.textContent = inputs[0].value;
     profileActivity.textContent = inputs[1].value;
