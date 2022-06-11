83355b81f22f1f2f3e16adae998edd7796956483
Darya Krinitsyna <krinitsyna.darya.98@mail.ru>
2022-06-11 17:43:48 +0300

    refact: обновлен файл JS

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
-let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popup__close-button');
+
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__about');
+
+let formElement = document.querySelector('.popup__form');
+let submitButton = formElement.querySelector('.popup__submit-button');
+let nameInput = formElement.querySelector('.name');
+let jobInput = formElement.querySelector('.about');
 
 let likeButtons = document.querySelectorAll('.element__like-button');
 
-let profileName = document.querySelector('.profile__name');
-
-let profileActivity = document.querySelector('.profile__about');
-let fieldName = document.querySelector('.name');
-let fieldActivity = document.querySelector('.about');
-let submitButton = document.querySelector('.popup__submit-button');
-let button;
-
 editButton.addEventListener('click', function () {
-    popup.classList.remove('hidden');
-    fieldName.value = profileName.textContent;
-    fieldActivity.value = profileActivity.textContent;
+    popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 })
 
-function addLike(button) {
-    button.classList.toggle('element__like-button_active');
-}
-
-
-likeButtons.forEach(function (likeButton) {
-    likeButton.addEventListener('click', () => addLike(likeButton)
-    );
-});
-
-
 closeButton.addEventListener('click', function () {
-    popup.classList.add('hidden');
+    popup.classList.remove('popup_opened');
 })
 
 submitButton.addEventListener('click', function (evt) {
     evt.preventDefault();
     let inputs = document.querySelectorAll('input');
     profileName.textContent = inputs[0].value;
-    profileActivity.textContent = inputs[1].value;
+    profileJob.textContent = inputs[1].value;
 })
+
+function addLike(button) {
+    button.classList.toggle('element__like-button_active');
+}
+
+likeButtons.forEach(function (likeButton) {
+    likeButton.addEventListener('click', () => addLike(likeButton)
+    );
+});
