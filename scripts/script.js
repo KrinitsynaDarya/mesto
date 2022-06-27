955256bcc4e2ebef52f6fd01f2e665ef7e51e382
Darya Krinitsyna <krinitsyna.darya.98@mail.ru>
2022-06-27 17:23:10 +0300

    feat: замена карточек на шаблонные при инициации

--- scripts/script.js
+++ scripts/script.js
@@ -42,3 +42,42 @@ likeButtons.forEach(function (likeButton) {
     likeButton.addEventListener('click', () => addLike(likeButton)
     );
 });*/
+
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const cardTemplate = document.querySelector('#element').content;
+const cardsGrid = document.querySelector('.elements');
+
+initialCards.forEach(function(card)
+{
+const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+cardElement.querySelector('.element__photo').src = card.link;
+cardElement.querySelector('.element__photo').alt = 'Фото ' + card.name;
+cardElement.querySelector('.element__title').textContent = card.name;
+cardsGrid.append(cardElement); 
+})
